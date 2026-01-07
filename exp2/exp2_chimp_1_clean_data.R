81d27f43473f4388ef9db3e01e1793a57a161b88
schmidt-daniela <daniela-schmidt@outlook.com>
2026-01-07 22:47:47 +0100

    copy paste scripts and functions from exp1 to exp2 (adjust later)

---
+++ exp2/exp2_chimp_1_clean_data.R
@@ -0,0 +1,322 @@
+## This script excludes invalid trials.
+## A trial is not valid, if an individual didn't look in the stimulus AOI for at least one fixation. 
+## Dec 01 2025 – Daniela Schmidt
+
+# General -----------------------------------------------------------------
+rm(list = ls())
+
+# Packages ----------------------------------------------------------------
+library(here)
+library(tidyverse)
+library(gazer) # remotes::install_github("dmirman/gazer")
+library(ggforce)
+library(readxl)
+
+# Set Parameters ----------------------------------------------------------
+folder <- "chimps"
+sample_size <- 17
+buffer <- 120 # 120px (3°) in chimps
+
+# Functions ---------------------------------------------------------------
+source(here("exp1", "R", "cleaning.R"))
+
+# Ape Information ---------------------------------------------------------
+apes_info <- read_excel(here("exp1", "doc", "information_apes.xlsx"))
+
+# Define AOIs -------------------------------------------------------------
+
+# Objects
+topobject_x_topleft <- 870 - buffer
+topobject_y_topleft <- 80 - buffer
+topobject_x_botright <- 1050 + buffer
+topobject_y_botright <- 260 + buffer
+
+botobject_x_topleft <- 870 - buffer
+botobject_y_topleft <- 820 - buffer
+botobject_x_botright <- 1050 + buffer
+botobject_y_botright <- 1000 + buffer
+
+# Popflakes
+topleftflake_x_topleft <- 380 - buffer
+topleftflake_y_topleft <- 170 - buffer
+topleftflake_x_botright <- 580 + buffer
+topleftflake_y_botright <- 370 + buffer
+
+botleftflake_x_topleft <- 380 - buffer
+botleftflake_y_topleft <- 710 - buffer
+botleftflake_x_botright <- 580 + buffer
+botleftflake_y_botright <- 910 + buffer
+
+toprightflake_x_topleft <- 1340 - buffer
+toprightflake_y_topleft <- 170 - buffer
+toprightflake_x_botright <- 1540 + buffer
+toprightflake_y_botright <- 370 + buffer
+
+botrightflake_x_topleft <- 1340 - buffer
+botrightflake_y_topleft <- 710 - buffer
+botrightflake_x_botright <- 1540 + buffer
+botrightflake_y_botright <- 910 + buffer
+
+centralflake_x_topleft <- 860 - buffer
+centralflake_y_topleft <- 440 - buffer
+centralflake_x_botright <- 1060 + buffer
+centralflake_y_botright <- 640 + buffer
+
+# Attention Getter
+at_x_topleft <- 863 - buffer
+at_y_topleft <- 443 - buffer
+at_x_botright <- 1057 + buffer
+at_y_botright <- 637 + buffer
+
+# Trial Exclusion ---------------------------------------------------------
+for(i in c(1:sample_size)){
+  
+  # Select file
+  filenames <- list.files(path = here("exp1", "data", "raw_2", folder))
+  n <- i
+  filename <- filenames[n]
+  
+  # Read file
+  raw <- read.table(here("exp1", "data", "raw_2", folder, filename), header = T, sep = "\t")
+  df <- raw
+  
+  # Add gaze-sample duration
+  df$gaze_sample_duration <- c(diff(df$Recording.timestamp), NA) / 1000
+  
+  # Tidy column names
+  df <- df |>  
+    rename_with(~ .x |> 
+                  str_replace_all("[^A-Za-z0-9]+", "_") |>  # replace everything that's not a number or a letter with _
+                  str_remove("^_+") |>  # remove one or more _ at the beginning of a string
+                  str_remove("_+$") |>  # remove one or more _ at the end of a string
+                  str_to_lower()) # make all letters lowercase
+  
+  # Remove columns that are not of interest and/or not informative
+  df <- df |> 
+    select(-c(ungrouped, client_area_position_x_dacspx, client_area_position_y_dacspx, viewport_position_x, # variables with NA only
+              viewport_position_y, viewport_width, viewport_height, full_page_width, full_page_height, # variables with NA only
+              mouse_position_x,mouse_position_y)) # variable not of interest
+  
+  # # Remove rows that are not of interest
+  # df <- df |> 
+  #   filter(presented_stimulus_name != "Eyetracker Calibration")
+  
+  # Prepare stimulus information
+  df <- df |>
+    separate(presented_stimulus_name,
+             into = c("session", "trial", "delete1", "stimulus", "stimulus_duration", "checkflake_location1", 
+                      "checkflake_location2", "delete2", "delete3", "object_identity", "object_location", "delete4"), 
+             remove = FALSE, sep = "_") |>
+    select(-c(delete1, delete2, delete3, delete4)) |> 
+    suppressWarnings()
+  
+  df <- df |>
+    mutate(object_location = if_else(stimulus %in% c("move", "still"), checkflake_location2, object_location))
+
+  # Set up trial and stimulus information
+  df <- df |> 
+    mutate(stimulus_duration = case_when(
+      stimulus == "move"  ~ "gaze_contingent",
+      stimulus == "still" ~ "1s",
+      stimulus == "at1"  ~ "gaze_contingent",
+      stimulus == "at2" ~ "1s",
+      stimulus_duration == "20s" ~ "gaze_contingent",
+      TRUE ~ stimulus_duration)) |> 
+    mutate(stimulus = case_when(
+      stimulus %in% c("move","still") ~ "object",
+      stimulus %in% c("at1","at2") ~ "at",
+      TRUE                            ~ stimulus)) |> 
+    mutate(position = case_when(
+      stimulus == "checkflake" & checkflake_location1 == "center" & checkflake_location2 == "center" ~ "center",
+      stimulus == "checkflake" ~ paste(checkflake_location1, checkflake_location2, sep = "_"),
+      object_location == "up" ~ "top",
+      object_location == "down" ~ "bottom",
+      stimulus == "object" ~ object_location,
+      stimulus == "at" ~ "center",
+      TRUE ~ object_location)) |> 
+    unite("stimulus_position", c("stimulus", "position"), sep = "_", remove = F)
+  
+  df[which(df$stimulus == "cueing"), "stimulus"] <- NA
+  df[which(df$stimulus |> str_detect("Kalei")), "stimulus"] <- NA
+  
+  df <- df |> select(-object_location, checkflake_location1, checkflake_location2) # redundant information
+  
+  df <- df |> 
+    group_by(recording_name) |> 
+    mutate(trial = make_trial_num(stimulus, stimulus_position),
+           trial = na_if(trial, 0L)) |> 
+    ungroup()
+  
+  df[is.na(df$stimulus),"trial"] <- NA
+  
+  # Add cumulative duration per trial
+  df <- df |> 
+    group_by(recording_name, trial, stimulus_duration) |> 
+    mutate(timeline_trial_units = cumsum(gaze_sample_duration)) |> 
+    group_by(recording_name, trial) |> 
+    mutate(timeline_trial_tot = cumsum(gaze_sample_duration)) |> 
+    ungroup()
+  
+  # Define AOIs (based on fixations)
+  df$aoi_fixation <- "not_in_aoi"
+  
+  df <- df |>
+    mutate(aoi_fixation = "not_in_aoi") |>
+    mark_aoi("top_left", topleftflake_x_topleft, topleftflake_x_botright, topleftflake_y_topleft, topleftflake_y_botright,
+             stimulus_name = "checkflake", position_name = "top_left", x_col = "fixation_point_x", y_col = "fixation_point_y",
+             aoi_col = "aoi_fixation") |>
+    mark_aoi("bot_left", botleftflake_x_topleft, botleftflake_x_botright, botleftflake_y_topleft, botleftflake_y_botright,
+             stimulus_name = "checkflake", position_name = "bot_left", x_col = "fixation_point_x", y_col = "fixation_point_y",
+             aoi_col = "aoi_fixation") |>
+    mark_aoi("top_right", toprightflake_x_topleft, toprightflake_x_botright, toprightflake_y_topleft, toprightflake_y_botright, 
+             stimulus_name = "checkflake", position_name = "top_right", x_col = "fixation_point_x", y_col = "fixation_point_y",
+             aoi_col = "aoi_fixation") |>
+    mark_aoi("bot_right",  botrightflake_x_topleft, botrightflake_x_botright, botrightflake_y_topleft,  botrightflake_y_botright, 
+             stimulus_name = "checkflake", position_name = "bot_right", x_col = "fixation_point_x", y_col = "fixation_point_y",
+             aoi_col = "aoi_fixation") |>
+    mark_aoi("center", centralflake_x_topleft, centralflake_x_botright, centralflake_y_topleft,  centralflake_y_botright, 
+             stimulus_name = "checkflake", position_name = "center", x_col = "fixation_point_x", y_col = "fixation_point_y",
+             aoi_col = "aoi_fixation") |>
+    mark_aoi("at", at_x_topleft, at_x_botright, at_y_topleft, at_y_botright,
+             stimulus_name = "at", position_name = "center", x_col = "fixation_point_x", y_col = "fixation_point_y",
+             aoi_col = "aoi_fixation") |>
+    mark_aoi("top", topobject_x_topleft, topobject_x_botright, topobject_y_topleft, topobject_y_botright, 
+             stimulus_name = "object", position_name = "top", x_col = "fixation_point_x", y_col = "fixation_point_y",
+             aoi_col = "aoi_fixation") |>
+    mark_aoi("bottom", botobject_x_topleft, botobject_x_botright, botobject_y_topleft, botobject_y_botright, 
+             stimulus_name = "object", position_name = "bottom", x_col = "fixation_point_x", y_col = "fixation_point_y",
+             aoi_col = "aoi_fixation")
+  
+  # Define AOIs (based on gaze samples)
+  df$aoi_samples <- "not_in_aoi"
+  
+  df <- df |>
+    mutate(aoi_samples = "not_in_aoi") |>
+    mark_aoi("top_left", topleftflake_x_topleft, topleftflake_x_botright, topleftflake_y_topleft, topleftflake_y_botright,
+             stimulus_name = "checkflake", position_name = "top_left", x_col = "gaze_point_x", y_col = "gaze_point_y",
+             aoi_col = "aoi_samples") |>
+    mark_aoi("bot_left", botleftflake_x_topleft, botleftflake_x_botright, botleftflake_y_topleft, botleftflake_y_botright,
+             stimulus_name = "checkflake", position_name = "bot_left", x_col = "gaze_point_x", y_col = "gaze_point_y",
+             aoi_col = "aoi_samples") |>
+    mark_aoi("top_right", toprightflake_x_topleft, toprightflake_x_botright, toprightflake_y_topleft, toprightflake_y_botright, 
+             stimulus_name = "checkflake", position_name = "top_right", x_col = "gaze_point_x", y_col = "gaze_point_y",
+             aoi_col = "aoi_samples") |>
+    mark_aoi("bot_right",  botrightflake_x_topleft, botrightflake_x_botright, botrightflake_y_topleft,  botrightflake_y_botright, 
+             stimulus_name = "checkflake", position_name = "bot_right", x_col = "gaze_point_x", y_col = "gaze_point_y",
+             aoi_col = "aoi_samples") |>
+    mark_aoi("center", centralflake_x_topleft, centralflake_x_botright, centralflake_y_topleft,  centralflake_y_botright, 
+             stimulus_name = "checkflake", position_name = "center", x_col = "gaze_point_x", y_col = "gaze_point_y",
+             aoi_col = "aoi_samples") |>
+    mark_aoi("at", at_x_topleft, at_x_botright, at_y_topleft, at_y_botright,
+             stimulus_name = "at", position_name = "center", x_col = "gaze_point_x", y_col = "gaze_point_y",
+             aoi_col = "aoi_samples") |>
+    mark_aoi("top", topobject_x_topleft, topobject_x_botright, topobject_y_topleft, topobject_y_botright, 
+             stimulus_name = "object", position_name = "top", x_col = "gaze_point_x", y_col = "gaze_point_y",
+             aoi_col = "aoi_samples") |>
+    mark_aoi("bottom", botobject_x_topleft, botobject_x_botright, botobject_y_topleft, botobject_y_botright, 
+             stimulus_name = "object", position_name = "bottom", x_col = "gaze_point_x", y_col = "gaze_point_y",
+             aoi_col = "aoi_samples")
+  
+  # Identify whether at least one fixation within AOI
+  fixation_in_aoi <- df |>
+    drop_na(stimulus) |>
+    filter(eye_movement_type == "Fixation") |>
+    select(recording_name, trial, stimulus_position, aoi_fixation) |>
+    filter(aoi_fixation != "not_in_aoi") |>
+    distinct() |>
+    select(recording_name, trial, stimulus_position) |>
+    mutate(excluded_fixation = "included")
+
+  df <- df |>
+    left_join(fixation_in_aoi, by = c("recording_name", "trial", "stimulus_position")) |>
+    mutate(excluded_fixation = replace_na(excluded_fixation, "excluded"))
+  
+  # Time within session
+  df <- df |> 
+    group_by(recording_name) |> 
+    mutate(timeline_experiment = cumsum(gaze_sample_duration)) |> 
+    ungroup()
+  
+  # Blink Detection (velocity-based)
+  df <- df |> 
+    rowwise() |> 
+    mutate(pupil_diameter_average = mean(c(pupil_diameter_left, pupil_diameter_right), na.rm = T)) |> 
+    ungroup()
+  
+  pupil_with_blinks <- annotate_blinks(
+    df = df,
+    nxmedian_neg_velocity_thresh = 4,
+    nxmedian_pos_velocity_thresh = 4,
+    max_blink_dur = 272,   # "Blink duration was normally distributed, with a mean of 153.5 ± 46.7 ms in the first test and 163.1 ± 42.3 ms in the second" (https://iovs.arvojournals.org/article.aspx?articleid=2188061)
+                           # 272ms is average blink duration in chimpanzees (https://journals.plos.org/plosone/article/file?id=10.1371/journal.pone.0066018&type=printable)
+    min_blink_dur = 10,    # In humans, he mean blink duration was 128.80ms (SD=56.4, range=10–347ms).(https://www.nature.com/articles/s41598-025-04839-y)
+    n_window = 3,
+    apply_moving_average = TRUE,
+    ma_window = 3)
+  
+  # Plot data for visual inspection of blinks
+  # Prepare data
+  pupil_with_blinks_plot <- pupil_with_blinks |>
+    group_by(participant_name, recording_name, trial, stimulus_duration) |>
+    mutate(first_valid_time = min(timeline_trial_units[!is.na(pupil_ma)], na.rm = TRUE)) |>     # find first time point with non-NA pupil_ma
+    filter(timeline_trial_units >= first_valid_time) |>     # drop any rows before that time, so the trial really "starts" there
+    mutate(time_in_trial0 = timeline_trial_units - first_valid_time) |>     # make that first valid time = 0
+    ungroup() |>
+    select(-first_valid_time)
+  
+  pupil_with_blinks_plot2 <- pupil_with_blinks_plot |>
+    group_by(participant_name, recording_name, trial, stimulus_duration) |>
+    mutate(blink_y = min(pupil_ma, na.rm = TRUE) - 0.05) |>   # constant line bit below the minimal pupil in this trial
+    ungroup()
+  
+  # Prepare Participant label for the title
+  participant_ids <- unique(pupil_with_blinks_plot$participant_name)
+  participant_label <- if (length(participant_ids) == 1L) {
+    participant_ids
+  } else {
+    paste(participant_ids, collapse = ", ")
+  }
+  
+  # Generate base plot (no facets or pagination yet)
+  p_base <- ggplot(pupil_with_blinks_plot2,
+                   aes(x = time_in_trial0, y = pupil_ma)) +
+    geom_line() +
+    geom_point(
+      data = pupil_with_blinks_plot2 |> filter(blink_detected == "yes"),
+      aes(x = time_in_trial0, y = blink_y),   # wichtig: y = blink_y!
+      inherit.aes = FALSE, color = "red", size = 0.2) + # red dots for all "blink_detected == "yes", independend of is.na(pupil)
+    labs(x = "Time in trial (s)", y = "Pupil diameter (average)") +
+    theme_minimal()
+  
+  # Define how many plots per pdf page
+  n_pages <- ggforce::n_pages(p_base +
+                                facet_wrap_paginate(~ recording_name + trial + stimulus_duration,
+                                                    ncol = 1, nrow = 1, scales = "free_x"))
+  
+  # Save all pages into one pdf file
+  pdf(here("exp1", "doc", "blink_check", paste0("pupil_blinks_", pupil_with_blinks$participant_name |> unique() |> tolower(), ".pdf")), width = 10, height = 7)
+  
+  for (i in seq_len(n_pages)) {
+    p_page <- p_base +
+      facet_wrap_paginate(~ recording_name + trial + stimulus_duration,
+                          ncol = 1, nrow = 1,
+                          scales = "free_x",
+                          page = i) +
+      ggtitle(paste0("Participant: ", participant_label,
+                     " — page ", i, " of ", n_pages))
+    print(p_page)
+  }
+  
+  dev.off()
+  
+  # Add information about ape
+  df_joined <- pupil_with_blinks |>  
+    left_join(apes_info, by = c("participant_name" = "id"))
+
+  # Write data
+  fname_base <- sub("\\.tsv$", "", filename)
+  out_rds <- here("exp1", "data", "raw_clean", folder, paste0(fname_base, ".rds"))
+  saveRDS(pupil_with_blinks, out_rds, compress = "xz")
+
+  print(pupil_with_blinks$recording_name |> unique())
+}
